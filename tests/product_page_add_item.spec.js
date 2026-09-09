bdf1c158d95367f5a3a0fd8682c4d3fd045e63fc
Naruf <102542599+Naruf@users.noreply.github.com>
2026-09-09 19:01:31 +0200

    Asserting before and after clicking behaviour

--- tests/product_page_add_item.spec.js
+++ tests/product_page_add_item.spec.js
@@ -1,13 +1,42 @@
-import { test } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
 test("Product Page Add To Basket", async ({ page }) => {
-  await page.goto("localhost:2221");
+  await page.goto("/");
 
   const addProductInCartButton = page
-    .getByRole("button", {
-      name: "Add to Basket",
-    })
+    .locator('[data-qa="product-button"]')
     .first();
+  const basketCount = page.locator('[data-qa="header-basket-count"]');
+  const checkoutElemnt = page.getByRole("link", { name: "Checkout" });
+  // const productCard = page
+  //   .locator("div")
+  //   .filter({
+  //     hasText:
+  //       /^Astronaut dabbingImage by catalyststuff on Freepik499\$Remove from basket$/,
+  //   })
+  //   .nth(1);
+  // const totalElement = page.getByText("Total: 499$");
+  // const continueToCheckOut = page.getByRole("button", {
+  //   name: "Continue to Checkout",
+  // });
+
+  await addProductInCartButton.waitFor();
+
+  await expect(addProductInCartButton).toHaveText("Add to Basket");
+  await expect(basketCount).toHaveText("0");
+
   await addProductInCartButton.click();
-  await page.pause();
+
+  await expect(addProductInCartButton).toHaveText("Remove from Basket");
+  await expect(basketCount).toHaveText("1");
+
+  await checkoutElemnt.waitFor();
+  await checkoutElemnt.click();
+  await page.waitForURL("/basket");
+
+  // await productCard.waitFor();
+  // await totalElement.waitFor();
+  // await continueToCheckOut.waitFor();
+
+  // await page.pause();
 });
