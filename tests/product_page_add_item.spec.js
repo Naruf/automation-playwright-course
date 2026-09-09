c4cd4215c61c4a1c6fbbcdacd51598daa242cad7
Naruf <102542599+Naruf@users.noreply.github.com>
2026-09-09 13:48:43 +0200

    Test for clicking on a button

--- tests/product_page_add_item.spec.js
+++ tests/product_page_add_item.spec.js
@@ -2,5 +2,12 @@ import { test } from "@playwright/test";
 
 test("Product Page Add To Basket", async ({ page }) => {
   await page.goto("localhost:2221");
+
+  const addProductInCartButton = page
+    .getByRole("button", {
+      name: "Add to Basket",
+    })
+    .first();
+  await addProductInCartButton.click();
   await page.pause();
 });
